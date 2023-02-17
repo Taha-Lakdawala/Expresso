ad65e0b24fa51093491b4044d015937ccca5027e
Taha <Tahalakdawala03@gmail.com>
2023-02-17 07:25:06 +0530

    added fetch chat API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const colors = require('colors')
 const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes')
 const { notFound, errorHandler } = require('../backend/middleware/errorMiddleware')
 
 const app = express();
@@ -15,6 +16,9 @@ const PORT = process.env.PORT
 app.use(express.json()); // To accept JSON data
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+
 app.use(notFound)
 app.use(errorHandler)
 
